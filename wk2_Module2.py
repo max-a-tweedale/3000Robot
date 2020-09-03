77324ae9a7437f080958c7ccb6bf287f59775418
Josh <joshgi0001@users.noreply.github.com>
2020-09-03 14:12:52 +1000

    first update
    Lets do this

---
+++ wk2_Module2.py
@@ -0,0 +1,49 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+class pen():
+    def __init__(self):
+        self.mode = 0
+        self.colour = 0
+        self.colours = ['red', 'green', 'blue']
+        self.modes = ['off', 'draw', 'erase']
+    def changeMode(self):
+        #When called toggles the mode
+        self.mode +=1
+        if self.mode > len(self.modes):
+            self.mode = 0
+        return self.modes[self.mode]
+
+    def changeColour(self):
+        #When called toggles the pen colour
+        self.colour +=1
+        if self.colour > len(self.colours):
+            self.colour = 0
+        return self.colours[self.colour]
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    #main
+
+
+
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+
+    key = cv2.waitKey(0) & 0xFF
+    if key == ord('d'):
+        #toggle draw, erase & off
+        changeMode()
+    elif key == ord('c'):
+        #toggle colout
+
+    if key == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
