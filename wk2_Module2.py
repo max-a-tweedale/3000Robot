f02aea53a103cdf94fff64aebffe78f13e484b1a
max-a-tweedale <max.a.tweedale@gmail.com>
2020-09-07 14:22:41 +1000

    Update wk2_Module2.py

--- wk2_Module2.py
+++ wk2_Module2.py
@@ -29,6 +29,7 @@ class PenController():
         self.colour = self.coloursRGB[self.colourInt]
         return self.coloursRGB[self.colourInt]
     
+
     def currentColour(self):
         #when called it returns touple of
         #string of colour ie 'red' and RGB value for current colour
@@ -42,23 +43,33 @@ while(True):
 
     #main
     
+
     #Mirror the frame for drawing easily
     frame = cv2.flip(frame,+1)
     
     #Convert the frame to grayscale and blur it slightly
+    #
+    # #Convert the frame to grayscale and blur it slightly
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray,(5,5),0)
-    
     #Threshold the image, apply erosions and dilations to remove small regions of noise
     #Threshold numbers may need to be changed to adapt to the ambient light
     thresh = cv2.threshold(gray,140,255,cv2.THRESH_BINARY)[1]
+    # gray = cv2.GaussianBlur(gray,(5,5),0)
+    #
+    # #Threshold the image, apply erosions and dilations to remove small regions of noise
+    # #Threshold numbers may need to be changed to adapt to the ambient light
+    thresh = cv2.threshold(gray,250,255,cv2.THRESH_BINARY)[1]
     thresh = cv2.erode(thresh,None,iterations=2)
     mask = cv2.dilate(thresh,None,iterations=2)
     
     #Apply the mask on the original image
+    #
+    # #Apply the mask on the original image
     combine = cv2.bitwise_and(frame,frame,mask=mask)
     
     #Find the hand and its centroid
+    #
+    # #Find the hand and its centroid
     _,contours,_ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
         if cv2.contourArea(contour)>1000:
@@ -68,6 +79,8 @@ while(True):
     cy = int(M['m01']/M['m00'])
     
     #Find the nib which is the toppest point of the finger
+
+    # #Find the nib which is the toppest point of the finger
     finger = tuple(hand[hand[:,:,1].argmin()][0])
     #Two numbers should be changed to adapted to the finger move
     #print(cx-finger[0]) or print(cy-finger[1]) to determined the range
@@ -75,20 +88,16 @@ while(True):
         x.append(finger[0])
         y.append(finger[1])
         
+    #
     #Drawing
     for i in range(len(x)):
         cv2.circle(frame,(x[i],y[i]),3,pen.colour,3)
-
-
-
-
     # Display the resulting frame
     cv2.imshow('frame', frame)
 
-    key = cv2.waitKey(0) & 0xFF
-   
 
     if key == ord('q'):
+
         break
 
 # When everything done, release the capture
