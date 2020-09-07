fc18f90e2500a4df0422b5acb4b0e8a1ad42b0b0
max-a-tweedale <max.a.tweedale@gmail.com>
2020-09-07 14:26:16 +1000

    Update balltrack.py

--- balltrack.py
+++ balltrack.py
@@ -17,6 +17,8 @@ args = vars(ap.parse_args())
 # define the lower and upper boundaries of the "green"
 # ball in the HSV color space, then initialize the
 # list of tracked points
+greenUpper = (255,247,0)
+greenLower = (153,169,51)
 pts = deque(maxlen=args["buffer"])
 # if a video path was not supplied, grab the reference
 # to the webcam
