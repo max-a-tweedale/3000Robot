448dd7e590b4441f207185c579c62f961cd0300d
max-a-tweedale <max.a.tweedale@gmail.com>
2020-09-07 14:25:58 +1000

    Changed waitkey(0) to waitkey(1)
    Fixes the bug that has only one frame show

--- wk2_Module2.py
+++ wk2_Module2.py
@@ -28,13 +28,12 @@ class PenController():
             self.colourInt = 0
         self.colour = self.coloursRGB[self.colourInt]
         return self.coloursRGB[self.colourInt]
-    
 
     def currentColour(self):
         #when called it returns touple of
         #string of colour ie 'red' and RGB value for current colour
         return self.colours[self.colourInt], self.coloursRGB[self.colourInt]
-    
+
 pen = PenController()
 
 while(True):
@@ -42,18 +41,12 @@ while(True):
     ret, frame = cap.read()
 
     #main
-    
 
     #Mirror the frame for drawing easily
     frame = cv2.flip(frame,+1)
-    
-    #Convert the frame to grayscale and blur it slightly
     #
     # #Convert the frame to grayscale and blur it slightly
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    #Threshold the image, apply erosions and dilations to remove small regions of noise
-    #Threshold numbers may need to be changed to adapt to the ambient light
-    thresh = cv2.threshold(gray,140,255,cv2.THRESH_BINARY)[1]
     # gray = cv2.GaussianBlur(gray,(5,5),0)
     #
     # #Threshold the image, apply erosions and dilations to remove small regions of noise
@@ -61,13 +54,9 @@ while(True):
     thresh = cv2.threshold(gray,250,255,cv2.THRESH_BINARY)[1]
     thresh = cv2.erode(thresh,None,iterations=2)
     mask = cv2.dilate(thresh,None,iterations=2)
-    
-    #Apply the mask on the original image
     #
     # #Apply the mask on the original image
     combine = cv2.bitwise_and(frame,frame,mask=mask)
-    
-    #Find the hand and its centroid
     #
     # #Find the hand and its centroid
     _,contours,_ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
@@ -77,8 +66,6 @@ while(True):
     M = cv2.moments(hand)
     cx = int(M['m10']/M['m00'])
     cy = int(M['m01']/M['m00'])
-    
-    #Find the nib which is the toppest point of the finger
 
     # #Find the nib which is the toppest point of the finger
     finger = tuple(hand[hand[:,:,1].argmin()][0])
@@ -87,13 +74,14 @@ while(True):
     if abs(cx - finger[0])<150 & abs(cy - finger[1])>50:
         x.append(finger[0])
         y.append(finger[1])
-        
     #
     #Drawing
     for i in range(len(x)):
         cv2.circle(frame,(x[i],y[i]),3,pen.colour,3)
     # Display the resulting frame
     cv2.imshow('frame', frame)
+
+    key = cv2.waitKey(1) & 0xFF
 
 
     if key == ord('q'):
